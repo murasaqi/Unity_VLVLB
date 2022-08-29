51e5bb29918bdaf7cce21711e5d1e058e8bd145b
murasaqi <uma9266@gmail.com>
2022-08-30 03:25:52 +0900

    wip

--- Assets/VLVLB/Editor/VLVLBTTimelineClipEditor.cs
+++ Assets/VLVLB/Editor/VLVLBTTimelineClipEditor.cs
@@ -2,7 +2,6 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using Unity.VisualScripting.YamlDotNet.Core.Tokens;
 using UnityEditor;
 using UnityEditor.Timeline;
 using UnityEngine;
